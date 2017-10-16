0e80d54631badca4badcb8209ae38a7845551cec
Hossam Emam <hossamsalahemam@gmail.com>
2017-10-16 20:43:32 +0200

    first commit with a bugy code

---
+++ matrix.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <curses.h>
+#include <ncurses.h>
+
+using namespace std;
+
+int main() {
+  std::cout << "hello, world!" << '\n';
+  getchar();
+  return 0;
+}
